358025c39c89b17e74e39939352b3f83a306098f
flamion <github@flamion.dev>
2025-09-26 14:20:51 +0200

    Implement functions to parse a single line

--- src/main.rs
+++ src/main.rs
@@ -39,14 +39,74 @@ pub fn rewe_extract_positions_lines(text: &str) -> Vec<&str> {
     positions
 }
 
+#[derive(Debug)]
 struct Position {
-    amount: u32,
     name: String,
+    amount: u32,
     price: f32,
 }
 
-pub fn rewe_extract_positions(lines: Vec<&str>) -> Vec<Position> {
-    todo!()
+fn get_price_indexes(line: &str) -> Option<(usize, usize)> {
+    let chars = line.char_indices().collect::<Vec<(usize, char)>>();
+
+    // ------------------- Extracting the price
+    let end_index = *chars.iter()
+        .rev()
+        .skip_while(|(_, c)| !c.is_digit(10))
+        .next()
+        .map(|(idx, _)| idx).unwrap();
+
+    let start_index = chars.iter()
+        .rev()
+        .skip_while(|(_, c)| *c != ',')
+        .skip_while(|(_, c)| !c.is_whitespace())
+        .next()
+        .map(|(idx, _)| idx + 1).unwrap(); // Currently pointing at the whitespace next to the number
+
+    Some((start_index, end_index))
+}
+
+fn parse_position_line(line: &str) -> Option<Position> {
+    let line = line.trim().to_owned();
+
+    let (start_index, end_index) = get_price_indexes(line.as_str())?;
+
+    let price = &mut line[start_index..=end_index].replace(',', ".");
+    let price: f32 = price.parse().unwrap();
+
+    // --------------------- Extracting the name of the product - shrimple now :shrimp:
+    let name = *&line[0..start_index].trim();
+
+    Some(Position {
+        amount: 1,
+        name: name.to_owned(),
+        price,
+    })
+}
+
+// Sample lines:
+// "KREUZKUEMMEL GEM                 1,99 B",
+// "RINDER FOND                      2,58 B",
+// "             2 Stk x    1,29",
+// "TOMATENMARK                      0,89 B",
+// "KIDNEYBOHNEN                     0,79 B",
+fn rewe_extract_positions(lines: Vec<&str>) -> Vec<Position> {
+    let mut positions = Vec::new();
+    let mut lines_iter = lines.into_iter().peekable();
+
+    while let Some(line) = lines_iter.next() {
+        let line = line.trim();
+        // Skip empty lines
+        if line.is_empty() {
+            continue;
+        }
+
+
+
+
+    }
+
+    positions
 }
 
 fn main() {
@@ -54,7 +114,9 @@ fn main() {
 
     let extracted_text = extract_text_from_pdf_with_qpdf(pdf_path).unwrap();
     //println!("{}", extracted_text);
-    let positions = rewe_extract_positions_lines(&extracted_text);
-    println!("{:#?}", positions);
+    let position_lines = rewe_extract_positions_lines(&extracted_text);
+    println!("{:#?}", position_lines);
 
+    let extracted_positions = rewe_extract_positions(position_lines);
+    println!("Positions: {:#?}", extracted_positions);
 }
