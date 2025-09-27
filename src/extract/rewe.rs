d3bd57daf0ffcf9c72411714672d0121434dc59e
flamion <contact@flamion.dev>
2025-09-27 20:35:37 +0200

    Move rewe functions into separate module

---
+++ src/extract/rewe.rs
@@ -0,0 +1,99 @@
+use regex::Regex;
+use crate::Position;
+
+fn get_price_indexes(line: &str) -> Option<(usize, usize)> {
+    let chars = line.char_indices().collect::<Vec<(usize, char)>>();
+
+    // ------------------- Extracting the price
+    let end_index = *chars.iter()
+        .rev()
+        .skip_while(|(_, c)| !c.is_ascii_digit())
+        .next()
+        .map(|(idx, _)| idx).unwrap();
+
+    let start_index = chars.iter()
+        .rev()
+        .skip_while(|(_, c)| *c != ',')
+        .skip_while(|(_, c)| !c.is_whitespace())
+        .next()
+        .map(|(idx, _)| idx + 1).unwrap(); // Currently pointing at the whitespace next to the number
+
+    Some((start_index, end_index))
+}
+
+fn parse_position_line(line: &str) -> Option<Position> {
+    let line = line.trim().to_owned();
+
+    let (start_index, end_index) = get_price_indexes(line.as_str())?;
+
+    let price = &mut line[start_index..=end_index].replace(',', ".");
+    let price: f32 = price.parse().unwrap();
+
+    // --------------------- Extracting the name of the product - shrimple now :shrimp:
+    let name = line[0..start_index].trim();
+
+    Some(Position {
+        amount: 1,
+        name: name.to_owned(),
+        price,
+    })
+}
+
+fn parse_amount(amount: &str) -> Option<u32> {
+    let amount = amount.trim();
+    let split = amount.split_once(' ').unwrap();
+    let amount: u32 = split.0.parse().unwrap();
+
+    Some(amount)
+}
+
+// Sample lines:
+// "KREUZKUEMMEL GEM                 1,99 B",
+// "RINDER FOND                      2,58 B",
+// "             2 Stk x    1,29",
+// "TOMATENMARK                      0,89 B",
+// "KIDNEYBOHNEN                     0,79 B",
+#[must_use]
+fn rewe_extract_positions(lines: Vec<&str>) -> Vec<Position> {
+    let mut positions: Vec<Position> = Vec::new();
+    let lines_iter = lines.into_iter().peekable();
+
+    let amount_pattern = Regex::new(r"^.*\d+\s+Stk\s+x\s+\d+,\d\d$").unwrap();
+    for line in lines_iter {
+        let line = line.trim();
+        // Skip empty lines
+        if line.is_empty() {
+            continue;
+        }
+
+        if amount_pattern.is_match(line) && !positions.is_empty() {
+            let new_amount = parse_amount(line).unwrap();
+            positions.last_mut().unwrap().amount = new_amount;
+            continue;
+        }
+
+        if let Some(position) = parse_position_line(line) {
+            positions.push(position);
+            continue;
+        }
+
+        panic!("Could not parse line {}", line);
+    }
+
+    positions
+}
+
+/// Extracts the lines containing positions from the REWE receipt text.
+fn rewe_extract_positions_lines(text: &str) -> Vec<&str> {
+    let lines = text.lines();
+
+    let lines = lines.skip_while(|line| line.trim() != "EUR").skip(1);
+
+
+    lines.take_while(|line| line.trim() != "--------------------------------------").collect()
+}
+
+pub fn extract_positions_from_rewe_text(text: &str) -> Vec<Position> {
+    let position_lines = rewe_extract_positions_lines(text);
+    rewe_extract_positions(position_lines)
+}
