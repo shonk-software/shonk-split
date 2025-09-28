e5a2d9cc7e795247830b77974c67ad9b435df99c
FlareFlo <mail@flareflo.dev>
2025-09-28 19:39:58 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use std::path::Path;
 use std::process::Command;
+use std::str::FromStr;
 use tempfile::NamedTempFile;
 
 /// Extracts text from a PDF, decrypting it with qpdf if needed.
@@ -35,10 +36,13 @@ pub fn rewe_extract_positions_lines(text: &str) -> Vec<&str> {
 
     let lines = lines.skip_while(|line| line.trim() != "EUR").skip(1);
 
-    let positions = lines.take_while(|line| line.trim() != "--------------------------------------").collect();
+    let positions = lines
+        .take_while(|line| line.trim() != "--------------------------------------")
+        .collect();
     positions
 }
 
+#[derive(Debug, Clone, PartialEq)]
 struct Position {
     amount: u32,
     name: String,
@@ -46,7 +50,51 @@ struct Position {
 }
 
 pub fn rewe_extract_positions(lines: Vec<&str>) -> Vec<Position> {
-    todo!()
+    let mut positions: Vec<Position> = Vec::new();
+    'outer: for (i, line) in lines.iter().enumerate() {
+        dbg!(line);
+        let split = line.rsplit(' ');
+        let mut name = None;
+        let mut price = None;
+        let mut qty = false;
+
+        for tok in split {
+            if qty {
+                positions.last_mut().unwrap().amount = u32::from_str(tok).unwrap();
+                continue 'outer;
+            }
+
+            // Try to parse price
+            if let Ok(realprice) = f32::from_str(&tok.replace(',', ".")) {
+                price = Some(realprice);
+                continue;
+            }
+
+            match tok {
+                "Stk" => {
+                    qty = true;
+                }
+                "B" | "A" | "*" | "x" | "" => {
+                    // Ignore
+                }
+                _ => {
+                    // Product name
+                    name = Some(
+                        format!("{tok} {}", name.unwrap_or_else(String::new))
+                            .trim()
+                            .to_owned(),
+                    );
+                }
+            }
+        }
+
+        positions.push(Position {
+            amount: 1,
+            name: name.unwrap(),
+            price: price.unwrap(),
+        });
+    }
+    positions
 }
 
 fn main() {
@@ -56,5 +104,5 @@ fn main() {
     //println!("{}", extracted_text);
     let positions = rewe_extract_positions_lines(&extracted_text);
     println!("{:#?}", positions);
-
+    dbg!(rewe_extract_positions(positions));
 }
